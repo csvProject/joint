2aa7cf5e5235be291f9bccda96d5f5a4f8d4cad6
wangkm <2239486439@qq.com>
2018-08-10 10:08:28 +0800

    Angular惰性加载（azyload）模块维护

---
+++ csvjava/src/main/angular/src/app/root/setting/setting-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SettingComponent } from './setting.component';
+import { TemplatesetComponent } from './templateset/templateset.component';
+import { PlatformacctsetComponent } from './platformacctset/platformacctset.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component:SettingComponent
+  },
+  {
+    path:'templateset',
+    component:TemplatesetComponent
+  },
+  {
+    path:'platformacctset',
+    component:PlatformacctsetComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class SettingRoutingModule { }
