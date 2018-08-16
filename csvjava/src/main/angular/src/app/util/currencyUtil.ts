6deea44574ed07105b338fbf51ec9487b31c2bfd
wangkm <2239486439@qq.com>
2018-08-16 12:02:40 +0800

    变量字段数据表

--- csvjava/src/main/angular/src/app/util/currencyUtil.ts
+++ csvjava/src/main/angular/src/app/util/currencyUtil.ts
@@ -124,4 +124,59 @@ export class CurrencyUtil {
     let range = selection.createRange ? selection.createRange() : selection.getRangeAt(0);
     return range;
   }
+
+
+  //时间格式化方法
+  //  let retTime = this.mms.dateFormat(new Date(),'yyyy-MM-dd');  // '2017-01-11'
+  dateFormat(inData?: Date, format?: string) {
+    if (inData == undefined || (inData + '') == '' || (inData + '') == 'null') {
+      return null;
+    }
+    let o = {
+      'M+': inData.getMonth() + 1,                 //月份
+      'd+': inData.getDate(),                    //日
+      'h+': inData.getHours(),                   //小时
+      'm+': inData.getMinutes(),                 //分
+      's+': inData.getSeconds(),                 //秒
+      'q+': Math.floor((inData.getMonth() + 3) / 3), //季度
+      'S': inData.getMilliseconds()             //毫秒
+    };
+    if (/(y+)/.test(format)) {
+      format = format.replace(RegExp.$1, (inData.getFullYear() + '').substr(4 - RegExp.$1.length));
+    }
+    for (let k in o) {
+      if (new RegExp('(' + k + ')').test(format)) {
+        format = format.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
+      }
+    }
+    return format;
+  }
+
+  //时间处理201801  -->  2018-01      20180102  -->  2018-01-02    splitSrc : - 或者 / 分割符
+  timeToTimeSrc(time, splitSrc?) {
+    if (splitSrc == undefined) {
+      splitSrc = '-';
+    }
+    let y = (time + '').substr(0, 2);
+    let timeTemp = (time + '').substr(2);
+    let timeList = timeTemp.split('');
+    let len = timeList.length;
+    if (len == 0) {
+      return '';
+    }
+    let timeSrc = '';
+    for (let i = 0; i < len; i++) {
+      if ((i + 1) % 2 == 0) {
+        if (i + 1 != len) {
+          timeSrc = timeSrc + timeList[i] + splitSrc;
+        } else {
+          timeSrc = timeSrc + timeList[i];
+        }
+      } else {
+        timeSrc = timeSrc + timeList[i];
+      }
+    }
+    timeSrc = y + timeSrc;
+    return timeSrc;
+  }
 }
