42f3e0b08e0d6b691069e1e7e817ca5e5dbfdafd
shopping <xuzp@linkstec.com>
2018-08-20 18:23:10 +0800

    模板字段表更新处理

--- csvjava/src/main/java/com/csv/java/entity/CsvTempBatDto.java
+++ csvjava/src/main/java/com/csv/java/entity/CsvTempBatDto.java
@@ -1,29 +1,15 @@
 package com.csv.java.entity;
 
+import java.util.List;
+
 //模板字段dto
-public class CsvTempBatDto extends BaseDto{
-    //CSV模板字段ID
-    private int csvFieldId;
+public class CsvTempBatDto extends BaseDto {
+
     //CSV模板ID
     private int csvtempId;
-    //模板字段key
-    private String fieldKey;
-    //模板字段key描述
-    private String fieldNm;
-    //模板字段value
-    private String fieldValue;
-    //模板字段类别
-    private String fieldType;
-    //模板字段顺序
-    private int fieldSort;
 
-    public int getCsvFieldId() {
-        return csvFieldId;
-    }
-
-    public void setCsvFieldId(int csvFieldId) {
-        this.csvFieldId = csvFieldId;
-    }
+    //csv字段dto
+    private List<CsvTemplateDetailDto> csvTemplateDetailDtoList;
 
     public int getCsvtempId() {
         return csvtempId;
@@ -33,43 +19,11 @@ public class CsvTempBatDto extends BaseDto{
         this.csvtempId = csvtempId;
     }
 
-    public String getFieldKey() {
-        return fieldKey;
+    public List<CsvTemplateDetailDto> getCsvTemplateDetailDtoList() {
+        return csvTemplateDetailDtoList;
     }
 
-    public void setFieldKey(String fieldKey) {
-        this.fieldKey = fieldKey;
-    }
-
-    public String getFieldNm() {
-        return fieldNm;
-    }
-
-    public void setFieldNm(String fieldNm) {
-        this.fieldNm = fieldNm;
-    }
-
-    public String getFieldValue() {
-        return fieldValue;
-    }
-
-    public void setFieldValue(String fieldValue) {
-        this.fieldValue = fieldValue;
-    }
-
-    public String getFieldType() {
-        return fieldType;
-    }
-
-    public void setFieldType(String fieldType) {
-        this.fieldType = fieldType;
-    }
-
-    public int getFieldSort() {
-        return fieldSort;
-    }
-
-    public void setFieldSort(int fieldSort) {
-        this.fieldSort = fieldSort;
+    public void setCsvTemplateDetailDtoList(List<CsvTemplateDetailDto> csvTemplateDetailDtoList) {
+        this.csvTemplateDetailDtoList = csvTemplateDetailDtoList;
     }
 }
