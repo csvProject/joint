297a7248f8b7d83e4780063a87d6c7ba18144bbd
wangkm <2239486439@qq.com>
2018-09-03 17:29:57 +0800

    csv导出1

---
+++ csvjava/src/main/java/com/csv/java/common/tool/CSVUtils.java
@@ -0,0 +1,68 @@
+package com.csv.java.common.tool;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.List;
+
+
+/**
+ * 创建CSV文件
+ */
+public class CSVUtils {
+
+    /**
+     * 创建CSV文件
+     */
+    public static void createCSV( List<Object> headList ,List<List<Object>> dataList,String fileName,String filePath) {
+        File csvFile = null;
+        BufferedWriter csvWtriter = null;
+        try {
+            csvFile = new File(filePath + fileName);
+            File parent = csvFile.getParentFile();
+            if (parent != null && !parent.exists()) {
+                parent.mkdirs();
+            }
+            csvFile.createNewFile();
+            // GB2312使正确读取分隔符","
+            csvWtriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(csvFile), "GB2312"), 1024);
+            int num = headList.size() / 2;
+            StringBuffer buffer = new StringBuffer();
+            for (int i = 0; i < num; i++) {
+                buffer.append(" ,");
+            }
+            // 写入文件头部
+            writeRow(headList, csvWtriter);
+            // 写入文件内容
+            for (List<Object> row : dataList) {
+                writeRow(row, csvWtriter);
+            }
+            csvWtriter.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                csvWtriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 写一行数据
+     * @param row 数据列表
+     * @param csvWriter
+     * @throws IOException
+     */
+    private static void writeRow(List<Object> row, BufferedWriter csvWriter) throws IOException {
+        for (Object data : row) {
+            StringBuffer sb = new StringBuffer();
+            String rowStr = sb.append("\"").append(data).append("\",").toString();
+            csvWriter.write(rowStr);
+        }
+        csvWriter.newLine();
+    }
+}
