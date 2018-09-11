cf897dc97ba47a5159af009547f28480cb7701a0
wangkm <2239486439@qq.com>
2018-09-11 15:18:52 +0800

    样式

--- csvjava/src/main/angular/src/environments/environment.ts
+++ csvjava/src/main/angular/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  uri:window.location.origin+'/',
+  uri:'localhost:8080'+'/',
   production: false
 };
 
