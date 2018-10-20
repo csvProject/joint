119f810af0609b0e267f714dc7fd70faf12b9fc3
shopping <xuzp@linkstec.com>
2018-10-20 11:49:44 +0800

    spring boot 定时器并发测试

--- csvjava/src/main/java/com/csv/java/config/ScheduledComponent.java
+++ csvjava/src/main/java/com/csv/java/config/ScheduledComponent.java
@@ -1,15 +1,15 @@
 package com.csv.java.config;
 
 import com.csv.java.OnlineDataService.OrderDataService;
+import com.csv.java.common.tool.DateUtil;
+import com.csv.java.common.tool.UUIDUtil;
 import com.csv.java.entity.FileDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import sun.plugin.util.UIUtil;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 import static com.csv.java.common.tool.DeleteFileUtil.deleteFile;
 import static com.csv.java.config.ConstantConfig.DELETE_ZIP_FILE_DAY;
@@ -44,4 +44,27 @@ public class ScheduledComponent {
     public void GenenateOrderDataFromMagento(){
         orderDataService.generateOrderDataFromMagento(10);
     }
+
+    /*
+    同步并发测试，每2秒执行
+     */
+    /*@Scheduled(cron = "0/4 * * * * ?")
+    public void test(){
+        System.out.println("测试开始:" + DateUtil.date2String(new Date(),"yyyy-MM-dd HH:mm:ss"));
+        System.out.println("UUID:" + UUIDUtil.getUUID());
+        try
+        {
+            Thread.currentThread().sleep(10000);//毫秒
+        }
+        catch(Exception e){}
+        *//*Timer timer=new Timer();//实例化Timer类
+
+        timer.schedule(new TimerTask(){
+                    public void run(){
+                        System.out.println("退出");
+                        this.cancel();
+                    }
+                },4000);//等待4秒*//*
+        System.out.println("我等了4秒:" + DateUtil.date2String(new Date(),"yyyy-MM-dd HH:mm:ss"));
+    }*/
 }
